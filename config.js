e008600b162c7f21ad30b5a11131e5844fff4f8b
glens-boom <glentech49@gmail.com>
2025-01-03 12:36:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "enter your session",
-    CAPTION: process.env.CAPTION || "code by yessertech ",
+    CAPTION: process.env.CAPTION || "code by glentech ",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
@@ -15,8 +15,8 @@ module.exports = {
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/3kdkf6.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M YESSER-MD WHATSAPP BOT 👍🤠",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/11sp6v.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "SASA MTU WANGU? MIMI NIKO ACTIVE KILA SAA ,NAITWA GLEN-MD WHATSAPP BOT🍒",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
@@ -27,7 +27,7 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "yessertech",
-    STATUS_REPLY: process.env.STATUS_REPLY || "`✅ your status seen 👀`",
+    BOT_NAME: process.env.BOT_NAME || "glentech",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`♥️your status seen successfully ♥️`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
